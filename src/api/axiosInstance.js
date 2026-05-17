3209243d0a0b1dad49a7d9c9418dd729f60c2dbe
Mohamedali409 <engmohamedali409@gmail.com>
2026-05-17 19:44:32 +0300

    .

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -2,8 +2,7 @@ import axios from "axios";
 export const apiURL = "http://localhost:3000";
 
 export const axiosInstance = axios.create({
-  baseURL:
-    "https://restaurant-pos-inven-git-d5c5cb-engmohamedali409-5913s-projects.vercel.app/",
+  baseURL: "http://localhost:3000/api/",
 });
 axiosInstance.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
